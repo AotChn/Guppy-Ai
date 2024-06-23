3ac3f74b19f1fc0becb5c331f76468d4659536d9
Aot Chanthorn <aotchanthorn@wifi-10-40-231-164.wifi.berkeley.edu>
2024-06-23 12:53:19 -0700

    version--3

--- whis.py
+++ whis.py
@@ -1,3 +1,9 @@
+from pywebio.input import input, FLOAT
+from pywebio.output import put_text
+from pywebio import start_server
+from pywebio.input import *
+from pywebio.output import *
+
 
 import openai
 import os 
@@ -5,104 +11,37 @@ import sounddevice as sd
 from scipy.io.wavfile import write
 import wavio as wv
 
-os.environ['OPENAI_API_KEY'] = ""
+x = select('ENTER', options=['ok'])
+
+put_text("WELCOME TO GUPPY")
+put_text("your virtual speech companion")
+
+
+
+x = select('Choose a prompt:', options=['Dog', 'Cats', 'Tree', 'Mountain', 'Beach', 'Bird', 'Flower', 'Car', 'House', 'River'])
+
+x = select('Select the amount of time you will get to read about your selected prompt. When the time is up, you will then present a short impromptu speech about your chosen prompt.', options=['20 seconds', '30 seconds', '60 seconds'])
+
+x = select('Select the amount of time you will get to present your impromptu speech.', options=['20 seconds', '30 seconds', '60 seconds'])
+
+os.environ['OPENAI_API_KEY'] = "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"
 
 client = openai.OpenAI()
-print("select a topic: ")
-print("[1] cats")
-print("[2] dogs")
-print("[3] rivers")
-print("[4] random")
-print("[5] custom")
-
-t = input("please select a topic: ")
-prepTime = int(input("select an amount to prep time: "))
-speechTime = int(input("select an amount to perform: "))
-
-
-if(t == "1"):
-    t = "cats"
-
-if(t == "2"):
-    t = "dogs"
-
-if(t == "3"):
-    t = "rivers"
-
-if(t == "4"):
-    t = "random"
-
-if(t == "5"):
-    t = input("enter custom topic: ")
-
-
-random_topics = [
-    "Artificial Intelligence",
-    "Space exploration",
-    "Cryptocurrency",
-    "Virtual reality",
-    "Climate change",
-    "Renewable energy",
-    "Neuroscience",
-    "Ancient civilizations",
-    "Robotics",
-    "Ethical hacking",
-    "Machine learning algorithms",
-    "Bioinformatics",
-    "Augmented reality",
-    "Internet of Things (IoT)",
-    "Quantum computing",
-    "3D printing technology",
-    "Future of work",
-    "Biotechnology",
-    "Urban planning",
-    "Marine biology",
-    "Astrobiology",
-    "Big data analytics",
-    "Nanotechnology",
-    "Cognitive psychology",
-    "Blockchain technology",
-    "Medical ethics",
-    "Cybersecurity threats",
-    "Digital marketing trends",
-    "Economic inequality",
-    "Geopolitics",
-    "Human rights movements",
-    "Cryptographic protocols",
-    "Data privacy laws",
-    "Genetic engineering",
-    "History of mathematics",
-    "Philosophy of mind",
-    "Sustainable agriculture",
-    "Virtual currencies",
-    "Space tourism",
-    "Artificial neural networks",
-    "Game theory applications",
-    "Environmental conservation",
-    "Music composition techniques",
-    "Remote sensing technologies",
-    "Cultural anthropology",
-    "Psychological disorders",
-    "Internet censorship",
-    "Ethics in technology",
-    "Human-computer interaction"
-]
-
-import random
-
-t.lower()
-if (t == 'random'):
-    t = str(random.choice(random_topics))
 
 def getTopic(topic):
     return "You are an expert with extensive knowledge in " + topic
 
-import time 
+response = client.chat.completions.create(
+  model="gpt-4o",
+  messages=[
+    {"role": "system", "content": getTopic("cats")},
+    {"role": "user", "content": "give 5 facts in your area of expertise as 5 short sentence with less than 7 words per sentence"}
+  ]
+)
 
-def fillSpace(r):
-    for i in range(r):
-        print(".\n")
+import time 
   
+# define the countdown func. 
 def countdown(t, endQuote): 
     
     while t: 
@@ -114,35 +53,26 @@ def countdown(t, endQuote):
       
     print(endQuote) 
 
-fillSpace(100)
-print("\n you be given ", prepTime, " to prep your impromtu speech on ", t)
-print("BE PREPARED STARTING IN 10 SECONDS")
-countdown(10, "prep time")
+put_text("HERE IS SOME INFO ABOUT CATS")
+put_text("*note you do not need to use this info in your speech :)")
+countdown(3,"")
 
-response = client.chat.completions.create(
-  model="gpt-4o",
-  messages=[
-    {"role": "system", "content": getTopic(t)},
-    {"role": "user", "content": "give 10 facts in your area of expertise as 5 short sentences that are less than 5 words each"}
-  ],
-  stream=True
-)
+put_text(response.choices[0].message.content)
 
-for chunk in response:
-    if chunk.choices[0].delta.content is not None:
-        print(chunk.choices[0].delta.content, end="")
+prepTime = 10
+speechTime = 20
 
-fillSpace(1)
-countdown(prepTime, "begin your speech!") 
 
-fillSpace(100)
+put_text("when prompted begin your speech")
+countdown(prepTime, "begin your speech!")
 
+put_text("BEGIN TALKING!")
 
 # Sampling frequency
 freq = 44100
 
 # Recording duration
-duration = 10
+duration = speechTime
 
 # Start recorder with the given values of 
 # duration and sample frequency
@@ -150,20 +80,25 @@ recording = sd.rec(int(duration * freq),
                    samplerate=freq, channels=1)
 
 # Record audio for the given number of seconds
-countdown(10, "Finished !")
+countdown(duration, "Finished !")
 sd.wait()
+put_text("[FINISHED!] ----- ANALYZING SPEECH ------------------")
 
+put_text("　　　 　　／＞　　 フ")
+put_text("　　　 　　| 　-　 -)  -- GOOD JOB !")
+put_text("　 　　 　／` ミ＿xノ")
+put_text("　　 　 /　　　 　 |")
+put_text("　　　 /　 ヽ　　 ﾉ")
+put_text("　 　 │　　|　|　|")
+put_text("　／￣|　　 |　|　|")
+put_text("　| (￣ヽ__ ヽ_) ヽ)")
+put_text("　＼二つ")
 # This will convert the NumPy array to an audio
 # file with the given sampling frequency
 write("recording0.wav", freq, recording)
 
 # Convert the NumPy array to audio file
 wv.write("recording1.wav", recording, freq, sampwidth=2)
-
-
-os.environ['OPENAI_API_KEY'] = "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"
-
-client = openai.OpenAI()
 
 audio_file = open("recording1.wav", "rb")
 result = client.audio.transcriptions.create(
@@ -173,44 +108,31 @@ result = client.audio.transcriptions.create(
   prompt="make all Um, um, uh, ums, eh, in the output"
 )
 
-placeholderWords = ["Um", "um", "uh", "ums", "eh", "yeah", "hmm", "hm", "ya"]
-print(result)
+placeholderWords = ["Um", "um", "uh", "ums", "eh", "yeah", "hmm", "hm", "ya", "Uh"]
+put_text("YOUR SPEECH: ")
+put_text(result)
+put_text("---------------------------------------")
+countdown(2, "")
 
 PLACEHOLDERS = 0
 for i in placeholderWords:
     if i in result:
         PLACEHOLDERS += 1
 
-print("place holder words detected : ", PLACEHOLDERS)
+put_text("place holder words detected : ", PLACEHOLDERS)
+if(PLACEHOLDERS > 0):
+    put_text("please be more concious of using place holder words such as um")
+countdown(2, "")
 
-rateSpeech = "given this speech give a rating from 0 to 100 in the cateories of structure, information relevance to " + t + " clarity, and overall score be a little friendly: " + result
+put_text("here are some things too look out for :)")
+rateSpeech = "given this speech give a rating from 0 to 100 in the cateories of structure, information relevance to " + "cats" + " clarity, and overall score: " + result
 
 coach = client.chat.completions.create(
   model="gpt-4o",
   messages=[
-    {"role": "system", "content": "You are a world renovned speech coach"},
+    {"role": "system", "content": "You are a world renowned speech coach"},
     {"role": "user", "content": rateSpeech}
-  ],
-  stream=True
+  ]
 )
 
-for chunk in coach:
-    if chunk.choices[0].delta.content is not None:
-        print(chunk.choices[0].delta.content, end="")
-
-
-def countWords(s):
-    # Check if the string is null
-    # or empty then return zero
-    if s.strip() == "":
-        return 0
-    # Splitting the string
-    words = s.split()
-    return len(words)
-
-def wordsPerMinute(words, s):
-    return words * (60/s)
-
-print("words in speech: ", countWords(result))
-print("words per minutes ", wordsPerMinute(countWords(result), speechTime))
-
+put_text(coach.choices[0].message.content)
